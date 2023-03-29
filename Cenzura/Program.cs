1c33088de8cbcc45058b21c923afe771e50665b3
DiyanTrifonov <diyantrifonov5@gmail.com>
2023-03-29 16:03:06 +0300

    First commit

---
+++ Cenzura/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cenzura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи дума, която да се цензурира: ");
+            string[] word = Console.ReadLine().Split().ToArray();
+            Console.WriteLine("Въведи изречение: ");
+            string izr = Console.ReadLine();
+
+            foreach (var item in word)
+            {
+                if (izr.Contains(item))
+                {
+                    izr = izr.Replace(item, new string('*', item.Length));
+                }
+            }
+            Console.WriteLine(izr);
+        }
+    }
+}
---
+++ RazklashtaneNaMerlah/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazklashtaneNaMerlah
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведи символен низ: ");
+            string input = Console.ReadLine();
+            Console.Write("Въведи шаблон за изтриване: ");
+            string template = Console.ReadLine();
+            string output = input;
+            byte couter = 0;
+
+            while (output.Contains(template))
+            {
+                couter++;
+                int pos = output.IndexOf(template);
+                int len = template.Length;
+                output = output.Remove(pos, len);
+            }
+
+            if (couter>=2)
+            {
+                Console.WriteLine("Shaked it!");
+            }
+            Console.WriteLine("No shake!");
+
+            Console.WriteLine($"Входящ низ: {input}");
+            Console.WriteLine($"Изходящ низ: {output}");
+        }
+    }
+}
